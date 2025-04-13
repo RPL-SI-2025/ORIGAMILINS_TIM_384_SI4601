9735331333a2e71d39fbfbe75f0683222ac6d28a
ayutri14 <ayutriy@gmail.com>
2025-04-13 13:06:22 +0700

    user seeder

--- SI4601_384_Origamilins/database/seeders/DatabaseSeeder.php
+++ SI4601_384_Origamilins/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +12,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // Admin
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'role' => 'admin',  
+        ]);
+
+        // User
+        User::factory()->create([
+            'name' => 'Ayu',
+            'email' => 'ayu@gmail.com',
+            'role' => 'user',  
+        ]);
+        User::factory()->create([
+            'name' => 'Alda',
+            'email' => 'alda@gmail.com',
+            'role' => 'user',  
+        ]);
+        User::factory()->create([
+            'name' => 'Hana',
+            'email' => 'hana@gmail.com',
+            'role' => 'user',  
+        ]);
+        User::factory()->create([
+            'name' => 'Rafli',
+            'email' => 'rafli@gmail.com',
+            'role' => 'user',  
+        ]);
+        User::factory()->create([
+            'name' => 'Fauzan',
+            'email' => 'fauzan@gmail.com',
+            'role' => 'user',  
+        ]);
+        User::factory()->create([
+            'name' => 'Nabil',
+            'email' => 'nabil@gmail.com',
+            'role' => 'user',  
         ]);
     }
 }
