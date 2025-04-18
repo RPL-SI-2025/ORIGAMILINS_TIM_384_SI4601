018ab00ee5ad4659a25c23dc2237775ae7a80714
ayutri14 <ayutriy@gmail.com>
2025-04-18 16:55:24 +0700

    produk.php

--- SI4601_384_Origamilins/app/Models/Produk.php
+++ SI4601_384_Origamilins/app/Models/Produk.php
@@ -8,11 +8,39 @@ class Produk extends Model
 {
     protected $table = 'produk';
 
+<<<<<<< Updated upstream
+=======
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'produk';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+>>>>>>> Stashed changes
     protected $fillable = [
         'nama',
         'harga',
         'kategori',
         'deskripsi',
+<<<<<<< Updated upstream
         'gambar',
+=======
+        'gambar'
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'harga' => 'decimal:2'
+>>>>>>> Stashed changes
     ];
 }
