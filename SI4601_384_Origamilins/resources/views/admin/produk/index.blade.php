212e8db934b020a21f3e0e2265cdd6dbf2e3f0a3
ayutri14 <ayutriy@gmail.com>
2025-04-18 21:37:32 +0700

    edit produk

--- SI4601_384_Origamilins/resources/views/admin/produk/index.blade.php
+++ SI4601_384_Origamilins/resources/views/admin/produk/index.blade.php
@@ -12,19 +12,12 @@
                     </a>
                 </div>
 
-<<<<<<< HEAD
-                @if(session('success'))
-                    <div class="alert alert-success">
-                        {{ session('success') }}
-=======
->>>>>>> c8cf72f0e2a873da259f8fec69e694eaedb5bca7
                 <div class="card-body">
                     @if(session('success'))
                         <div class="alert alert-success">
                             {{ session('success') }}
                         </div>
                     @endif
-<<<<<<< HEAD
 
                     <div class="table-responsive">
                         <table class="table table-bordered table-striped">
@@ -70,64 +63,6 @@
                                                 @csrf
                                                 @method('DELETE')
                                                 <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus produk ini?')">
-                                                    <i class="fas fa-trash"></i> Delete
-                                                </button>
-                                            </form>
-                                        </div>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                @endif
-=======
->>>>>>> c8cf72f0e2a873da259f8fec69e694eaedb5bca7
-
-                    <div class="table-responsive">
-                        <table class="table table-bordered table-striped">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Gambar</th>
-                                    <th>Nama Produk</th>
-                                    <th>Harga</th>
-                                    <th>Kategori</th>
-                                    <th>Deskripsi</th>
-                                    <th>Aksi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($products as $product)
-                                <tr>
-                                    <td>{{ $product->id }}</td>
-                                    <td>
-                                        @if($product->gambar)
-                                            @if(filter_var($product->gambar, FILTER_VALIDATE_URL))
-                                                <img src="{{ $product->gambar }}" alt="{{ $product->nama }}" class="img-thumbnail" style="max-width: 100px;">
-                                            @else
-                                                <img src="{{ asset('storage/' . $product->gambar) }}" alt="{{ $product->nama }}" class="img-thumbnail" style="max-width: 100px;">
-                                            @endif
-                                        @else
-                                            <span class="text-muted">No Image</span>
-                                        @endif
-                                    </td>
-                                    <td>{{ $product->nama }}</td>
-                                    <td>Rp {{ number_format($product->harga, 0, ',', '.') }}</td>
-                                    <td>{{ $product->kategori }}</td>
-                                    <td>{{ $product->deskripsi }}</td>
-                                    <td>
-                                        <div class="btn-group">
-                                            <a href="#" class="btn btn-info btn-sm">
-                                                <i class="fas fa-eye"></i> View
-                                            </a>
-                                            <a href="#" class="btn btn-warning btn-sm">
-                                                <i class="fas fa-edit"></i> Edit
-                                            </a>
-                                            <form action="#" method="POST" class="d-inline">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?')">
                                                     <i class="fas fa-trash"></i> Delete
                                                 </button>
                                             </form>
